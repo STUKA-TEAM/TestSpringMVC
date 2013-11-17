4b76ebaf518f3e83742985f2043f4882c3931dec
BelieveIt <yucheng.byc@qq.com>
2013-11-17 14:45:55 +0800

    Spring MVC Controller example

---
+++ src/TestController/HelloWorldController.java
@@ -0,0 +1,17 @@
+package TestController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping(value = "/helloworld", method = RequestMethod.GET)
+    public String helloWorld(ModelMap modelMap){
+    	String messageString  = "HELLO WORLD";
+    	modelMap.addAttribute("message", messageString);
+    	return "helloWorldView";
+    }
+}
